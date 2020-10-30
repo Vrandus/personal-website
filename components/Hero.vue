83e73d4d88522c2b15b2e4458db6746793c583f2
Prateek T <lolcrosell@gmail.com>
2020-10-29 23:23:01 -0400

    added hero

---
+++ components/Hero.vue
@@ -0,0 +1,49 @@
+<template> 
+     <section class="hero is-info is-large">
+          <div class="hero-body">
+               <div class="container">
+                    <h1 class="title">
+                    {{ title }}
+                    </h1>
+                    <h2 class="subtitle">
+                    {{ subtitle }}
+                    </h2>
+               </div>
+          </div>
+     </section>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    subtitle: {
+      type: String,
+      required: true,
+    },
+  },
+}
+</script>
+
+<style>
+     .hero{
+          height: 50%;
+          background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url("photographer.jpg");
+          background-position: center;
+          background-repeat: no-repeat;
+          background-size: cover;
+          position: relative;
+
+     }
+     .title {
+          margin-left: 4rem;
+          font-size: 4rem;
+     }
+     .subtitle{
+          margin-left: 6rem;
+          opacity: 60%;
+     }
+</style>
